4199a0675e1e55736012f51a82aa1bfb30e3e692
simon-o3h <simon@oooh.tv>
2022-08-01 17:09:15 +0100

    Updating translations site

---
+++ translations/assets/translations.js
@@ -0,0 +1,19 @@
+var translations = {
+    "languages": {
+        "en": "English",
+        "es": "Español",
+        "de": "Deutsch"
+    },
+    "elementLangIds": {
+        "titleSub": {
+            "en": "Personal Wellness Coaching En",
+            "es": "Personal Wellness Coaching Es",
+            "de": "Personal Wellness Coaching De"
+        },
+        "titleBtn": {
+            "en": "Get Started En",
+            "es": "Get Started Es",
+            "de": "Get Started De"
+        }
+    }
+}
