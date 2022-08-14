48fe6669ad017e094fac4b0904e154d968b0e8e1
fitspirit <107228597+fitspirit@users.noreply.github.com>
2022-08-14 10:05:27 +0200

    Update translations.js

--- translations/assets/translations.js
+++ translations/assets/translations.js
@@ -16,8 +16,17 @@ var translations = {
             "de": "Los geht's!"
         },
         "intro_p1": {
-            "en": "Introduction in English",
-            "es": "indtroduccion en espanol",
+            "en": "En Your wellbeing is the focus of my craft.
+                    I love to encourage my clients and taylor my efforts to your individual desires.
+                    We develop a solution that uniquely fits your needs.
+                    <br/><br/>
+                    As a former engineer, my approach is systematic and science-based.
+                    My strong analytic abilities allow me to understand your situation at the core.
+                    We will find an individual approach, that is focused on what motivates you.
+                    Our goal: Create positive emotions and a lasting change.
+                    Body - Mind - Spirit: Three aspects of that should be addressed in an integrated approach.
+                    Together, we'll develop a holistic plan to reach your goals.",
+            "es": "",
             "de": "Übersetzung auf Detusch"
         }
     }
